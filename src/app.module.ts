70cebb9cf7b2660a53907268b00fabfab544a3e1
jaylton <jayltonokra15@gmail.com>
2025-12-09 22:38:33 -0300

    reativando auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { UserModule } from './infrastructure/http/modules/user.module';
 import { CardModule } from './infrastructure/http/modules/card.module';
 import { CategoryModule } from './infrastructure/http/modules/category.module';
 import { AccountsModule } from './modules/accounts/accounts.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, CardModule, CategoryModule, AccountsModule],
+  imports: [UserModule, PrismaModule, CardModule, CategoryModule, AccountsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,25 +1,25 @@
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import { PrismaService } from 'src/prisma/prisma.service';
-// import * as bcrypt from 'bcrypt';
-// import { JwtService } from '@nestjs/jwt';
-// import { User } from '@prisma/client';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 
-// @Injectable()
-// export class AuthService {
+@Injectable()
+export class AuthService {
 
-//     constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {}
+    constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {}
 
-//     async validateUser(email: string, password: string) {
-//         const user = await this.prisma.user.findUnique({ where: { email } });
+    async validateUser(email: string, password: string) {
+        const user = await this.prisma.user.findUnique({ where: { email } });
 
-//         if (user && (await bcrypt.compare(password, user.password))) {
-//             return await this.login(user);
-//         }
-//         throw new UnauthorizedException('Invalid credentials');
-//     }
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return await this.login(user);
+        }
+        throw new UnauthorizedException('Invalid credentials');
+    }
 
-//     async login(user: User) {
-//         return this.jwtService.signAsync({ sub: user.id, email: user.email });
-//     }
+    async login(user: User) {
+        return this.jwtService.signAsync({ sub: user.id, email: user.email });
+    }
 
-// }
+}
